327f6dc13fceac1f2248f110741e231fa427008a
mateuszrozek <mateusz.s.rozek@gmail.com>
2020-10-14 21:58:12 +0200

    MealService tests

--- src/test/java/pl/rozekm/yummy/mealapi/GetCategoriesTest.java
+++ src/test/java/pl/rozekm/yummy/mealapi/GetCategoriesTest.java
@@ -1,4 +1,37 @@
 package pl.rozekm.yummy.mealapi;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class GetCategoriesTest {
+
+    private static final String CATEGORY = "Seafood";
+    @Autowired
+    private MealService mealService;
+
+    @Test
+    public void shouldGetCategories(){
+        List<CategoryApiDto> categories = mealService.getCategories();
+
+        assertNotNull(categories);
+        assertFalse(categories.isEmpty());
+    }
+
+    @Test
+    public void shouldGetByCategory(){
+        List<MealApiDto> categories = mealService.getByCategory(CATEGORY);
+
+        assertNotNull(categories);
+        assertFalse(categories.isEmpty());
+    }
 }
